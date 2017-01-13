803ae205e7dc9bb2722b8551d2a5fe38789086f2
Steve-Burton <stevevburton@gmail.com>
2017-01-13 09:14:33 -0800

    add functionality to view individual questions

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,13 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestionForm: false,
   actions: {
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
     update(question) {
       var params = {
         author: this.get('author'),
         question: this.get('question'),
         notes: this.get('notes')
       };
+      this.set('updateQuestionForm', false);
       this.sendAction('update', question, params);
     }
   }
